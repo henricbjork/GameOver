fe89a4fdaeda0bfe9c07ab9cfb77dabf56868c30
Henric <henric.bjorkvall@gmail.com>
2020-04-30 11:50:13 +0200

    day one

--- pipe.js
+++ pipe.js
@@ -8,11 +8,11 @@ class Pipe {
   }
 
   hits(bird) {
-      if (bird.y < this.top || bird.y > height - this.bottom) {
-          if (bird.x > this.x && bird.x < this.x + this.w) {
-            return true;
-          }
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        return true;
       }
+    }
 
     return false;
   }
